2f8b84628966a7be52f1f183bf17338074c420e4
quochuy1133 <130635660+quochuy1133@users.noreply.github.com>
2025-04-25 23:17:10 +0700

    Update BienNguyHiemVaCanhBaoFragment.java
    by Hoang Quoc Huy

---
+++ On-thi-GPLX-main/app/src/main/java/com/laptrinhandroid/on_thi_gplx/activity/BienNguyHiemVaCanhBaoFragment.java
@@ -0,0 +1,56 @@
+package com.laptrinhandroid.on_thi_gplx.activity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.laptrinhandroid.on_thi_gplx.R;
+import com.laptrinhandroid.on_thi_gplx.adapter.BienBaoAdapter;
+import com.laptrinhandroid.on_thi_gplx.utilities.DbHelper;
+import com.laptrinhandroid.on_thi_gplx.model.RoadSign;
+
+import java.util.List;
+
+public class BienNguyHiemVaCanhBaoFragment extends Fragment {
+    private Context mContext;
+    RecyclerView recyclerView;
+    List<RoadSign> list;
+    DbHelper helper;
+    BienBaoAdapter adapter;
+
+    public BienNguyHiemVaCanhBaoFragment() {
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mContext = context;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        helper = new DbHelper(mContext);
+        return inflater.inflate(R.layout.fragment_bien_nguy_hiem_va_canh_bao, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        recyclerView = view.findViewById(R.id.listBienNguyHiem);
+        list = helper.getRSWithRSID(4);
+        adapter = new BienBaoAdapter(list, mContext);
+        RecyclerView.ItemDecoration decoration = new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(decoration);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setHasFixedSize(true);
+    }
+}
